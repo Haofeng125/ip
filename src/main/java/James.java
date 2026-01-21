a83fb0843eb1435b2c837987408c97c53dd387e4
Haofeng125 <e1484139@u.nus.edu>
2026-01-21 20:25:58 +0800

    Add support for echo, personalized my chatbox

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class James {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm James");
-        System.out.println("What can I do for you?");
-        System.out.println("______________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("______________________________________________________________");
+        String divider = "  ____________________________________________________________";
+
+        System.out.println(divider);
+        System.out.println("  Wassup! This is James.");
+        System.out.println("  What can I do for you?");
+        System.out.println(divider);
+
+        Scanner sc = new Scanner (System.in);
+
+        while (true) {
+            String input = sc.nextLine();
+            System.out.println(divider);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("  You take care bro. Hope to see you again soon!");
+                System.out.println(divider);
+                break;
+            }
+
+            System.out.println("  " + input);
+            System.out.println(divider);
+        }
+        sc.close();
     }
 }
