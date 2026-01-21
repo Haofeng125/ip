91182fa0ae66b587b850f6aaed0d7ed1dbf21505
Haofeng125 <e1484139@u.nus.edu>
2026-01-21 19:47:43 +0800

    Rename main class, add greatings

---
+++ src/main/java/James.java
@@ -0,0 +1,10 @@
+public class James {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm James");
+        System.out.println("What can I do for you?");
+        System.out.println("______________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________________________________________________");
+    }
+}
