9a6c74e27d84f048b1f929bb26e37b50eef18733
Haofeng125 <e1484139@u.nus.edu>
2026-01-21 22:55:50 +0800

    Add the ability to mark tasks as done. Optionally, add the ability to change the status back to not done.

--- src/main/java/James.java
+++ src/main/java/James.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class James {
     public static void main(String[] args) {
         String divider = "  ____________________________________________________________";
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         System.out.println(divider);
         System.out.println("  Wassup! This is James.");
@@ -14,6 +14,7 @@ public class James {
 
         while (true) {
             String input = sc.nextLine();
+            String[] words = input.split(" ");
             System.out.println(divider);
 
             if (input.equalsIgnoreCase("bye")) {
@@ -21,11 +22,46 @@ public class James {
                 System.out.println(divider);
                 break;
             }else if (input.equalsIgnoreCase("list")) {
+                System.out.println("  Here you go bro! Here's your list.");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i + 1) + ". " + list.get(i));
+                    System.out.println("  " + (i + 1) + "." + list.get(i).toString());
+                }
+            } else if (words[0].equalsIgnoreCase("mark")) {
+                if (words.length < 2) {
+                    System.out.println("  Bro, you need to tell me which number to mark!");
+                } else {
+                    try {
+                        int number = Integer.parseInt(words[1]) - 1;
+                        if (number >= 0 && number < list.size()) {
+                            list.get(number).mark();
+                            System.out.println("  No problem man! I've marked this task as done:");
+                            System.out.println("    " + list.get(number).toString());
+                        } else {
+                            System.out.println("  Nah bro, that task number doesn't exist!");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("  Chill, that's not a number! Use something like 'mark 1'.");
+                    }
+                }
+            } else if (words[0].equalsIgnoreCase("unmark")) {
+                if (words.length < 2) {
+                    System.out.println("  Bro, you need to tell me which number to unmark!");
+                } else {
+                    try {
+                        int number = Integer.parseInt(words[1]) - 1;
+                        if (number >= 0 && number < list.size()) {
+                            list.get(number).unmark();
+                            System.out.println("  I got you bro! I've marked this task as not done yet:");
+                            System.out.println("    " + list.get(number).toString());
+                        } else {
+                            System.out.println("  Nah bro, that task number doesn't exist!");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("  Chill, that's not a number! Use something like 'unmark 1'.");
+                    }
                 }
             }else {
-                list.add(input);
+                list.add(new Task(input));
                 System.out.println("  added: " + input);
             }
             System.out.println(divider);
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " ");
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String str = String.format("[%s] %s", this.getStatusIcon(), this.description );
+        return str;
+    }
+}
